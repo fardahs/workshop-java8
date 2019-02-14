110a7e06a719b5206852b55d8f8c2c0c4befd355
Farda HOUMADI <farda.houmadi@hotmail.fr>
2019-02-14 16:22:31 +0100

    stream exo 1 a 3

--- src/test/java/java8/ex02/Stream_02_Test.java
+++ src/test/java/java8/ex02/Stream_02_Test.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 
 import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.Matchers.*;
@@ -24,7 +25,10 @@ public class Stream_02_Test {
         List<Order> orders = new Data().getOrders();
 
         // Trouver la liste des clients ayant déjà passés une commande
-        List<Customer> result = null;
+        List<Customer> result = orders.stream()
+        		.map(client -> client.getCustomer())
+        		.distinct()
+        		.collect(Collectors.toList());
 
         assertThat(result, hasSize(2));
     }
@@ -36,7 +40,11 @@ public class Stream_02_Test {
 
         // TODO calculer les statistiques sur les prix des pizzas vendues
         // TODO utiliser l'opération summaryStatistics
-        IntSummaryStatistics result = null;
+        IntSummaryStatistics result = orders.stream()
+        		.flatMap(prix -> prix.getPizzas()
+        				.stream()
+        				.map(price -> price.getPrice()))
+        		.collect(IntSummaryStatistics::new, IntSummaryStatistics::accept, IntSummaryStatistics::combine);
 
 
         assertThat(result.getSum(), is(10900L));
