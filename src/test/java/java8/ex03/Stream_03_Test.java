096c1d0d7ffec1a9863b7c4a6a52559d454ea604
Farda HOUMADI <farda.houmadi@hotmail.fr>
2019-02-14 17:34:00 +0100

    map exo3

--- src/test/java/java8/ex03/Stream_03_Test.java
+++ src/test/java/java8/ex03/Stream_03_Test.java
@@ -69,7 +69,10 @@ public class Stream_03_Test {
         List<Customer> customers = new Data().getCustomers();
 
         // TODO Construire la map Sexe -> Chaîne représentant les prénoms des clients
-        Map<Gender, String> result = null;
+        Map<Gender, String> result = customers.stream()
+        		.sorted(Comparator.comparing(Customer::getFirstname))
+        		.collect(Collectors.groupingBy(Customer::getGender, 
+        				Collectors.mapping(Customer::getFirstname, Collectors.joining("|"))));
 
         assertThat(result.get(Gender.F), is("Alexandra|Marion|Sophie"));
         assertThat(result.get(Gender.M), is("Cyril|Johnny"));
