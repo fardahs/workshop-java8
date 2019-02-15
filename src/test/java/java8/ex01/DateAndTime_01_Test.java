7f92337ffd2356188039dab327a5d62460963e38
Houmadi farda <farda!.houmadi@hotmail.fr>
2019-02-15 08:11:07 +0100

    date exo 1 a 7

--- src/test/java/java8/ex01/DateAndTime_01_Test.java
+++ src/test/java/java8/ex01/DateAndTime_01_Test.java
@@ -14,16 +14,19 @@ import static org.junit.Assert.assertThat;
 public class DateAndTime_01_Test {
 
     // Vous vous souvenez de java.util.Date (Java 1) ?
-    @Test
+    @SuppressWarnings("deprecation")
+	@Test
     public void test_date() {
 
         // TODO modifier les paramètres pour que le test soit passant
-        Date date = new Date(0, 0, 0, 0,0,0);
+        Date date = new Date(2017-1900, 3-1, 24, 01,02,03);
+        
 
         assertThat(date.toString(), is("Fri Mar 24 01:02:03 CET 2017"));
 
         // une date est mutable... :-(
         // TODO modifier le jour de la date pour que le test soit passant
+        date.setDate(25);
 
         assertThat(date.toString(), is("Sat Mar 25 01:02:03 CET 2017"));
     }
@@ -33,14 +36,22 @@ public class DateAndTime_01_Test {
     @Test
     public void test_calendar() throws Exception {
         Calendar calendar = Calendar.getInstance();
+        calendar.set(2017, 3-1, 24, 01, 02, 03);
+        
+//        calendar.set(Calendar.YEAR, 2017);
+//        calendar.set(Calendar.MONTH, 3-1);
+//        calendar.set(Calendar.DAY_OF_MONTH, 24);
+//        calendar.set(Calendar.HOUR_OF_DAY, 01);
+//        calendar.set(Calendar.SECOND, 02);
+//        calendar.set(Calendar.MILLISECOND, 03);
 
         // TODO modifier l'objet calendar pour que le test soit passant
-
+        
         assertThat(calendar.getTime().toString(), is("Fri Mar 24 01:02:03 CET 2017"));
 
         // calendar est mutable... :-(
         // TODO modifier l'objet calendar pour que le test soit passant
-
+        calendar.set(Calendar.DAY_OF_MONTH, 25);
         assertThat(calendar.getTime().toString(), is("Sat Mar 25 01:02:03 CET 2017"));
     }
 
